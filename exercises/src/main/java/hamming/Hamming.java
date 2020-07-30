872e2e9bffc78272acb5abb808f44c241330f4b3
Jakub Kwintal <jakubkwintal@yahoo.com>
2020-07-30 22:07:00 +0200

    Hamming

--- exercises/src/main/java/hamming/Hamming.java
+++ exercises/src/main/java/hamming/Hamming.java
@@ -3,17 +3,14 @@ package hamming;
 public class Hamming {
     private int hammingDistance = 0;
 
-    public Hamming(String leftStrand, String rightStrand) throws IllegalArgumentException {
-        IllegalArgumentException e1 = new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
-        IllegalArgumentException e2 = new IllegalArgumentException("left strand must not be empty.");
-        IllegalArgumentException e3 = new IllegalArgumentException("right strand must not be empty.");
+    public Hamming(String leftStrand, String rightStrand) {
 
         if ((leftStrand.length() != rightStrand.length()) && !leftStrand.isEmpty() && !rightStrand.isEmpty()) {
-            throw e1;
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
         } else if (leftStrand.isEmpty() && !rightStrand.isEmpty()) {
-            throw e2;
+            throw new IllegalArgumentException("left strand must not be empty.");
         } else if (rightStrand.isEmpty() && !leftStrand.isEmpty()) {
-            throw e3;
+            throw new IllegalArgumentException("right strand must not be empty.");
         } else {
             for (int i = 0; i < leftStrand.length(); i++) {
                 if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
