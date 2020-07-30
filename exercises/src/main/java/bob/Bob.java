5803ff70a6eb6bc3680e229f38056a8b5f29859e
Jakub Kwintal <jakubkwintal@yahoo.com>
2020-07-30 15:25:39 +0200

    House

--- exercises/src/main/java/bob/Bob.java
+++ exercises/src/main/java/bob/Bob.java
@@ -1,4 +1,36 @@
 package bob;
 
+import java.util.regex.Pattern;
+
 public class Bob {
+
+    private static boolean isUpperCase(String input) {
+        char[] inputArray = input.toCharArray();
+
+        for (char c : inputArray) {
+            if (Character.isLetter(c)) {
+                if (!Character.isUpperCase(c)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public String hey(String input) {
+
+        String finalInput = input.replaceAll("\\s", "");
+
+        if (finalInput.isEmpty()) {
+            return "Fine. Be that way!";
+        } else if (isUpperCase(finalInput) && Pattern.compile("[a-zA-Z]").matcher(finalInput).find()) {
+            if (!finalInput.endsWith("?")) {
+            return "Whoa, chill out!";}
+            else {return "Calm down, I know what I'm doing!";}
+        } else if (finalInput.endsWith("?")) {
+            return "Sure.";
+        } else {
+            return "Whatever.";
+        }
+    }
 }
--- exercises/src/main/java/house/House.java
+++ exercises/src/main/java/house/House.java
@@ -1,2 +1,54 @@
-package house;public class House {
+package house;
+
+public class House {
+
+    static String[] firstPartOfLine = {"that lay in",
+            "that ate",
+            "that killed",
+            "that worried",
+            "that tossed",
+            "that milked",
+            "that kissed",
+            "that married",
+            "that woke",
+            "that kept",
+            "that belonged to"};
+
+    static String[] secondPartOfLine = {"the house that Jack built.",
+            "the malt",
+            "the rat",
+            "the cat",
+            "the dog",
+            "the cow with the crumpled horn",
+            "the maiden all forlorn",
+            "the man all tattered and torn",
+            "the priest all shaven and shorn",
+            "the rooster that crowed in the morn",
+            "the farmer sowing his corn",
+            "the horse and the hound and the horn"};
+
+    public static String verse(int verseNumber) {
+
+        StringBuilder oneVerse = new StringBuilder("This is " + secondPartOfLine[verseNumber - 1]);
+
+        for (int i = verseNumber - 1; i > 0; i--) {
+            oneVerse.append(" ").append(firstPartOfLine[i - 1]).append(" ").append(secondPartOfLine[i - 1]);
+        }
+        return oneVerse.toString();
+    }
+
+
+    public String verses(int startVerse, int endVerse) {
+        StringBuilder verses = new StringBuilder();
+
+        for (int i = startVerse; i <= endVerse; i++) {
+            verses.append(verse(i)).append("\n");
+        }
+        return verses.deleteCharAt(verses.length() - 1).toString();
+    }
+
+
+    public String sing() {
+        return verses(1, secondPartOfLine.length);
+    }
 }
